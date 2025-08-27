45c8230175820e678cf212fa2f754c188565aee6
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-08-27 12:41:29 +1000

    [FIX] waybar service

--- home-manager/modules/waybar/service.nix
+++ home-manager/modules/waybar/service.nix
@@ -3,6 +3,7 @@
   systemd.user.services.waybar = {
       Unit = {
         Description="waybar";
+        After = [ "graphical-session.target" ];
       };
 
       Install = {
@@ -13,11 +14,6 @@
         ExecStart = "${pkgs.waybar}/bin/waybar";
         Restart = "on-failure";
         Type = "simple";
-        Environment = [
-          "DISPLAY=:0"
-          "XDG_RUNTIME_DIR=/run/user/%U"
-          "PATH=${pkgs.lib.makeBinPath [ pkgs.gpu-screen-recorder ]}:${pkgs.alacritty}"
-        ];
       };
   };
 }
