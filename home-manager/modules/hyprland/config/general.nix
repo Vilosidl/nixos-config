e4897ebd759b5dbf60c9b5b3f910e8499d59825f
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-08-27 10:28:54 +1000

    [CHANGE & FIX] col.active_border and remove mistake waybar/style.css

--- home-manager/modules/hyprland/config/general.nix
+++ home-manager/modules/hyprland/config/general.nix
@@ -5,7 +5,7 @@
       gaps_in = 5;
       gaps_out = 10;
       border_size = 2;
-      "col.active_border" = lib.mkDefault "rgb(d3c6aa)";
+      "col.active_border" = lib.mkDefault "rgb(939ab7)";
       "col.inactive_border" = lib.mkDefault "rgba(595959aa)";
       resize_on_border = false;
       allow_tearing = true;
