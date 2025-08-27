d4e4baab6fc7c2a6eab00938199c68f29fd59402
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-08-27 11:14:57 +1000

    [UPDATE] fileSystems.nix add SSD

--- nixos/modules/fileSystems.nix
+++ nixos/modules/fileSystems.nix
@@ -3,4 +3,9 @@
     device = "/dev/disk/by-uuid/03e3b0f4-ef22-4d80-b317-c30c8a15140c";
     fsType = "ext4";
   };
+
+  fileSystems."/mnt/SSD" = {
+    device = "/dev/disk/by-uuid/6a95ed75-d10d-4ba8-a1a7-306f0a7325e6";
+    fsType = "ext4";
+  };
 }
