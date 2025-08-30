799f85d39217fee9d78ebfbda3ecbe957bc1888f
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-08-30 16:49:41 +1000

    [UPDATE] nix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,3 +1,9 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 }
