a877af9b4767fe0aa219a8fd5636c1595a63fb50
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-08-31 08:54:13 +1000

    [UPDATE] hyprland

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -5,11 +5,4 @@
   };
 
   programs.uwsm.enable = true;
-  programs.uwsm.waylandCompositors = {
-  hyprland = {
-    prettyName = "Hyprland";
-    comment = "Hyprland compositor managed by UWSM";
-    binPath = "/run/current-system/sw/bin/Hyprland";
-  };
-  };
 }
