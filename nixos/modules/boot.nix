1b1f795343b0f978bb99a420ec4af258699de1a5
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-09-02 15:22:59 +1000

    [REFACTOR] kernel and boot

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   boot.initrd.availableKernelModules = [
     "nvidia_drm" "nvidia_modeset" "nvidia" "nvidia_uvm"
   ];
   boot.loader.timeout = 1;
+
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = true;
+    "net.core.rmem_max" = 7500000;
+    "net.core.wmem_max" = 7500000;
+  };
 }
