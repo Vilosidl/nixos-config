87f12866a54869a5ebf2fbd97deb36702397745c
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-09-02 19:12:29 +1000

    [UPDATE] pycharm

--- home-manager/modules/pycharm/default.nix
+++ home-manager/modules/pycharm/default.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   version = "PyCharm2025.1";
 in
@@ -5,37 +6,50 @@ in
   home.file = {
     ".config/JetBrains/${version}/plugin_PCWMP.license" = {
       enable = true;
-      source = ./config/plugin_PCWMP.license;
+      source = ./source/config/plugin_PCWMP.license;
+      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PBASHSUPPORTPRO.license" = {
       enable = true;
-      source = ./config/plugin_PBASHSUPPORTPRO.license;
+      source = ./source/config/plugin_PBASHSUPPORTPRO.license;
+      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PNGINX.license" = {
       enable = true;
-      source = ./config/plugin_PNGINX.license;
+      source = ./source/config/plugin_PNGINX.license;
+      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PRAINBOWBRACKET.license" = {
       enable = true;
-      source = ./config/plugin_PRAINBOWBRACKET.license;
+      source = ./source/config/plugin_PRAINBOWBRACKET.license;
+      recursive = true;
     };
 
     ".config/JetBrains/${version}/pycharm.key" = {
       enable = true;
-      source = ./config/pycharm.key;
+      source = ./source/config/pycharm.key;
+      recursive = true;
     };
 
     ".config/JetBrains/${version}/options/colors.scheme.xml" = {
       enable = true;
-      source = ./config/colors.scheme.xml;
+      source = ./source/config/colors.scheme.xml;
+      recursive = true;
+    };
+
+    ".config/JetBrains/${version}/options/laf.xml" = {
+      enable = true;
+      source = ./source/config/laf.xml;
+      recursive = true;
     };
 
     ".local/share/JetBrains/${version}" = {
       enable = true;
-      source = ./local;
+      source = ./source/local;
+      recursive = true;
     };
   };
 }
