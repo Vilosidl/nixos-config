4d59ddfb78c50c2c63f3f26a13aa68f92d1e3e15
Dovi <221439051+Vilosidl@users.noreply.github.com>
2025-09-03 16:16:04 +1000

    [UPDATE] pycharm

--- home-manager/modules/pycharm/default.nix
+++ home-manager/modules/pycharm/default.nix
@@ -7,43 +7,36 @@ in
     ".config/JetBrains/${version}/plugin_PCWMP.license" = {
       enable = true;
       source = ./source/config/plugin_PCWMP.license;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PBASHSUPPORTPRO.license" = {
       enable = true;
       source = ./source/config/plugin_PBASHSUPPORTPRO.license;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PNGINX.license" = {
       enable = true;
       source = ./source/config/plugin_PNGINX.license;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/plugin_PRAINBOWBRACKET.license" = {
       enable = true;
       source = ./source/config/plugin_PRAINBOWBRACKET.license;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/pycharm.key" = {
       enable = true;
       source = ./source/config/pycharm.key;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/options/colors.scheme.xml" = {
       enable = true;
       source = ./source/config/colors.scheme.xml;
-      recursive = true;
     };
 
     ".config/JetBrains/${version}/options/laf.xml" = {
       enable = true;
       source = ./source/config/laf.xml;
-      recursive = true;
     };
 
     ".local/share/JetBrains/${version}" = {
